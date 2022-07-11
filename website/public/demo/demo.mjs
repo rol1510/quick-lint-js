d77c7a8e1155a0f1982f79323d913ed3698b917f
Matthew "strager" Glazar <strager.nds@gmail.com>
2022-07-10 21:32:14 -0700

    @@@ redo web demo marking

--- website/public/demo/demo.mjs
+++ website/public/demo/demo.mjs
@@ -6,6 +6,7 @@ import { markEditorText } from "./editor.mjs";
 import {} from "../error-box.mjs";
 
 let codeInputElement = document.getElementById("code-input");
+let codeInputMarksElement = document.getElementById("code-input-marks");
 let shadowCodeInputElement = document.getElementById("shadow-code-input");
 
 codeInputElement.addEventListener("scroll", (event) => {
@@ -15,6 +16,61 @@ synchronizeContent();
 
 if (typeof window.ResizeObserver !== "undefined") {
   new window.ResizeObserver(synchronizeSize).observe(codeInputElement);
+}
+
+class FloatingEditorMarker {
+  constructor(editor, marksContainer) {
+    this._markElements = [];
+    this._editor = editor;
+    this._marksContainer = marksContainer;
+  }
+
+  setMarks(marks, text) {
+    let textNode = this._editor.childNodes[0];
+    if (typeof textNode === 'undefined') {
+      return;
+    }
+
+    let marksContainerRect = this._marksContainer.getBoundingClientRect();
+    let offsetX = -marksContainerRect.left;
+    let offsetY = -marksContainerRect.top;
+
+    let range = window.document.createRange();
+    let markElementIndex = 0;
+    for (let mark of marks) {
+      range.setStart(textNode, mark.begin);
+      range.setEnd(textNode, mark.end);
+      let rects = range.getClientRects();
+      for (let r of rects) {
+        let markElement = this._getOrCreateMark(markElementIndex++);
+        markElement.style.display = 'inline';
+        markElement.style.left = `${r.left + offsetX}px`;
+        markElement.style.top = `${r.top + offsetY}px`;
+        markElement.style.width = `${r.width}px`;
+        markElement.style.height = `${r.height}px`;
+        markElement.setAttribute("data-message", mark.message);
+        markElement.setAttribute("data-code", mark.code);
+        markElement.setAttribute("data-severity", mark.severity);
+        markElement.textContent = text.slice(mark.begin, mark.end);
+      }
+    }
+
+    for (let i = markElementIndex; i < this._markElements.length; ++i) {
+      this._markElements[i].style.display = 'none';
+    }
+  }
+
+  _getOrCreateMark(markElementIndex) {
+    return markElementIndex >= this._markElements.length ? this._createMark() : this._markElements[markElementIndex];
+  }
+
+  _createMark() {
+    let markElement = document.createElement('mark');
+    markElement.classList.add('floating-mark');
+    this._marksContainer.appendChild(markElement);
+    this._markElements.push(markElement);
+    return markElement;
+  }
 }
 
 createProcessFactoryAsync()
@@ -43,6 +99,8 @@ createProcessFactoryAsync()
       });
     }
 
+    let marker = new FloatingEditorMarker(shadowCodeInputElement, codeInputMarksElement);
+
     function lintAndUpdate() {
       if (doc === null) {
         restartProcessThenLint();
@@ -62,7 +120,7 @@ createProcessFactoryAsync()
         marks = [];
         processCrashed();
       }
-      markEditorText(shadowCodeInputElement, window, marks);
+      marker.setMarks(marks, input);
     }
     codeInputElement.addEventListener("input", (event) => {
       lintAndUpdate();
